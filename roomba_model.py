ed0a9455fdd0721c3bfba56002a12845fa38d960
Cesar Flores <A01751101@tec.mx>
2024-11-08 19:09:15 -0600

    Start

---
+++ roomba_model.py
@@ -0,0 +1,98 @@
+import mesa
+
+class RoombaAgent(mesa.Agent):
+    def __init__(self, name, model):
+        # Pass the parameters to the parent class.
+        super().__init__(name, model)
+
+        # Create the agent's variable and set the initial values.
+        self.stepsGiven = 0
+        self.overlapTimes = 0
+
+    def move(self):
+        possible_steps = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,
+            include_center=False)
+        new_position = self.random.choice(possible_steps)
+        self.model.grid.move_agent(self, new_position)
+
+    def clean(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        if len(cellmates) > 1:
+            for agent in cellmates:
+                if isinstance(agent, ManchaAgent):
+                    print("clean roomba")
+                    agent.clean()
+                    break
+
+    def checkClean(self):
+        cellmates = self.model.grid.get_cell_list_contents([self.pos])
+        for agent in cellmates:
+            if isinstance(agent, ManchaAgent):
+                if not agent.is_clean:
+                    return False
+        return True
+
+    def step(self):
+        if not self.checkClean():
+            self.clean()
+        else:
+            self.move()
+
+class ManchaAgent(mesa.Agent):
+    def __init__(self, name, model):
+        # Pass the parameters to the parent class.
+        super().__init__(name, model)
+
+        # Create the agent's variable and set the initial values.
+        self.is_clean = False
+
+    def clean(self):
+        if not self.is_clean:
+            self.is_clean = True
+            print('Mancha cleaned')
+
+class RoombaModel(mesa.Model):
+    def __init__(self, N, width, height, sucio, t_max):
+        super().__init__()
+        self.num_agents = N
+        self.grid = mesa.space.MultiGrid(width, height, True)
+        self.p_sucio = sucio
+        self.t_max = t_max
+        # Create scheduler and assign it to the model
+        self.schedule = mesa.time.RandomActivation(self)
+        self.running = True
+        # Create agents
+        for i in range(self.num_agents):
+            a = RoombaAgent(i, self)
+            # Add the agent to the scheduler
+            self.schedule.add(a)
+            # Add the agent to the starting grid cell (1, 1)
+            self.grid.place_agent(a, (1, 1))
+
+        for i in range(int(width * height * (self.p_sucio/100))):
+            a = ManchaAgent(i, self)
+            self.schedule.add(a)
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            cellmates = self.grid.get_cell_list_contents([(x, y)])
+            while not len (cellmates) == 0:
+                x = self.random.randrange(self.grid.width)
+                y = self.random.randrange(self.grid.height)
+                cellmates = self.grid.get_cell_list_contents([(x, y)])
+            self.grid.place_agent(a, (x, y))
+
+    def checkManchas(self):
+        for agent in self.schedule.agents:
+            if isinstance(agent, ManchaAgent):
+                if not agent.is_clean:
+                    return False
+        return True
+
+    def step(self):
+        print(self.schedule.steps)
+        if self.schedule.steps >= self.t_max-1 or self.checkManchas():
+            self.running = False
+        else:
+            self.schedule.step()
---
+++ run.py
@@ -0,0 +1,34 @@
+from mesa_viz_tornado.ModularVisualization import ModularServer
+from mesa_viz_tornado.UserParam import NumberInput, Slider
+
+from roomba_model import mesa, RoombaModel, RoombaAgent, ManchaAgent
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "roomba.png",
+                 "Filled": "true",
+                 "Layer": 0,
+                 "r": 0.5}
+    if isinstance(agent, ManchaAgent):
+        portrayal["Shape"] = "mancha.png"
+        portrayal["r"] = 0.75
+        if agent.is_clean:
+            portrayal["Shape"] = "circle"
+            portrayal["Color"] = "white"
+
+    return portrayal
+
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+
+model_params = { "N": NumberInput("Number of Roombas", value=10),
+                 "width": Slider("Grid Width", 10, 5, 999, 1),
+                 "height": Slider("Grid Height", 10, 5, 999, 1),
+                 "sucio": Slider("Percentage of Dirty Spots", 10, 0, 100, 1),
+                 "t_max": Slider("Max Steps", 100, 1, 999, 1) }
+
+server = ModularServer(
+    RoombaModel,
+    [grid],
+    "Roomba Model",
+    model_params )
+server.port = 8521 # The default
+server.launch()
